8e0ec4b0091b7c9752b6d306b84f723c988af148
AyalaSomech <as7674404@gmail.com>
2025-01-09 01:03:41 +0200

    I18next

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 // import Logo from './component/logo1';
 import ImagesPass from './lesson_2/component/click'
-import { NavbarBrand } from 'react-bootstrap';
+import { NavbarBrand} from 'react-bootstrap';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from "./project/Pages/Home";
 import About from "./project/Pages/About";
 import Connect from "./project/Pages/Connect";
 import Services from "./project/Pages/Services";
 import TenckYou from "./project/component/TenckYou"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// import "container-fluid"
+// import 'navbar navbar-expand-lg bg-body-tertiary';
 
 
 function App() {
@@ -17,12 +22,14 @@ function App() {
 
   {/* <ImagesPass></ImagesPass> */}
   <BrowserRouter>
-  <nav>
-    <Link to="/">Home</Link> | 
-    <Link to="/about">About</Link> | 
-    <Link to="/connect">Connect</Link> | 
-    <Link to="/services">Services</Link> | 
-  </nav>
+  <NavbarBrand className='navbar navbar-expand-lg bg-body-tertiary'>
+    <div >
+    <Link to="/" className='nav-link' >Home</Link> 
+    <Link to="/about" className='nav-link'>About</Link>
+        <Link to="/connect" className='nav-link'>Connect</Link> 
+    <Link to="/services" className='nav-link'>Services</Link> 
+    </div>
+  </NavbarBrand>
   <Routes>
     <Route path='/' element={<Home />}/>
     <Route path='/about' element={<About />}/>
---
+++ src/i18n.js
@@ -0,0 +1,11 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import {getLocales} from 'react-native-localize';
+i18n.use(initReactI18next).init({
+    lng: getLocales()[0].languageCode ,fallbackLng: 'en', resources:
+    {
+        en:{
+            
+        }
+    }
+})
